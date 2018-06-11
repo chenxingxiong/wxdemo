d1f03b5f24fc3f1e9f49f74b5571ab29971f0d27
axing <xingzizailushang@163.com>
2018-06-11 15:26:10 +0800

    动态loading data

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,10 +1,57 @@
 //index.js
 //获取应用实例
 const app = getApp()
+const weatherMap={
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+
+}
+const weatherColorMap={
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
+}
 
 Page({
+  data:{
+    nowTemp:'',
+    nowWeather:"",
+    nowWeatherBgimg:''
+  },
   onLoad: function () {
     console.log("hello world");
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data:{city:"上海市"},
+     // success:function(res){
+      //  console.log(res);
+    //  }
+    success:res=>{
+      console.log(res);
+      let result=res.data.result;
+      let nowTemp=result.now.temp;
+      let weather=result.now.weather;
+      this.setData({
+        nowTemp:nowTemp,
+        nowWeather: weatherMap[weather],
+        nowWeatherBgimg: '/img/' + weather+'-bg.png'
+      })
+      wx.setNavigationBarColor({
+        frontColor: '#000000',
+        backgroundColor: weatherColorMap[weather],
+      })
+       
+    }
+      
+      
+    })
 
   }     
   
