9f15c741e205e7c77f0a11a654864e758216b997
axing <chenxingxiong@huaxiafinance.com>
2018-07-09 14:35:51 +0800

    获取列表页数据

--- pages/list/list.js
+++ pages/list/list.js
@@ -1,10 +1,13 @@
 // pages/list/list.js
+const dayMap=['星期日','星期一','星期二','星期三','星期四','星期五','星期六']
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    weekWeather:[]
   
   },
 
@@ -12,37 +15,10 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.getWeekWeather()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
+   
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
@@ -50,17 +26,42 @@ Page({
   
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
   
+
+   
+
+  getWeekWeather(){
+   wx.request({
+     url: 'https://test-miniprogram.com/api/weather/future',
+     data:{
+       city:'上海',
+       time:new Date().getTime()
+     },
+     success:res=>{
+       let result=res.data.result;
+       console.log(result);
+       this.setWeekWeather(result)
+     }
+   })
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  setWeekWeather(result){
+    let weekWeather=[]
+    for(let i=0;i<7;i++){
+      let date=new Date()
+      date.setDate(date.getDate()+i)
+      weekWeather.push({
+        day:dayMap[date.getDay()],
+        date:date.getFullYear()+'-'+`${date.getMonth()+1}`+'-'+date.getDate(),
+        temp:result[i].minTemp+"-"+result[i].maxTemp,
+        iconPath:'/img/'+result[i].weather+'-icon.png'
+      })
+
+    }
+    weekWeather[0].day='今天',
+    this.setData({
+      weekWeather:weekWeather
+    })
   }
+
 })
